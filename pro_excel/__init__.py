2efd9add73b0f70787e4bb01aca78f67dd65a5cd
“felixyh” <felixyanghui@gmail.com>
2022-10-19 14:44:46 +0800

    update

--- pro_excel/__init__.py
+++ pro_excel/__init__.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+    @app.route('/index')
+    def index():
+        return 'hello, I am Flask running on code-server!'
 
     return app
